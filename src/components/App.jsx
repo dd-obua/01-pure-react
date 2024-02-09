5a7315263117672cb06490d6111ad8bc49894fc9
Obua Denis Daniel <dd.obua@gmail.com>
2024-02-09 17:13:47 +0300

    Set text and css class conditionally

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -82,15 +82,13 @@ const Menu = () => {
 };
 
 const Pizza = ({ pizzaObj }) => {
-  if (pizzaObj.soldOut) return null;
-
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''}`}>
       <img src={pizzaObj.photo} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price}</span>
       </div>
     </li>
   );
