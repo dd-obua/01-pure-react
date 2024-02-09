f139854b7d286a7aa5a0780f3ddaba8e0cd284ee
Obua Denis Daniel <dd.obua@gmail.com>
2024-02-09 17:00:35 +0300

    Use a react fragment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -62,11 +62,18 @@ const Menu = () => {
       <h2>Our menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisaine. 6 creative dishes to choose from. All from our
+            stone oven, all organic, all delicious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We are still working on our menu. Please come back later :)</p>
       )}
